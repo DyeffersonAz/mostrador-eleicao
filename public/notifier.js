6899064709dccde31732b572392d9ae1d6b72dd8
Dyefferson Azevedo <dyeffersonazevedo@hotmail.com>
2020-11-14 16:12:46 -0300

    New notification system 🔔

--- public/notifier.js
+++ public/notifier.js
@@ -10,6 +10,15 @@ async function notifyElection(title, body) {
   }
   const img = 'images/party.png';
   new Notification(title, {body: body, icon: img});
+  Toastify({
+  text: body,
+  duration: 5000, 
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: 'right', // `left`, `center` or `right`
+  backgroundColor: "linear-gradient(to right, #10561F, #256F15)",
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+}).showToast();
 }
 
 /**
@@ -23,6 +32,15 @@ async function notifyMatematicamente(title, body) {
   }
   const img = 'images/warning.png';
   new Notification(title, {body: body, icon: img});
+  Toastify({
+  text: body,
+  duration: 5000, 
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: 'right', // `left`, `center` or `right`
+  backgroundColor: "linear-gradient(to right, #FE7914, #FFDD55)",
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+}).showToast();
 }
 
 /**
